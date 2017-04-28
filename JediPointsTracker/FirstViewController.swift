395ee4c6900ce3f10d3e5350164bb670911b70ce
Amanda Chappell <Amanda.Chappell@wnco.com>
2017-04-28 10:30:53 -0500

    Initial Commit

---
+++ JediPointsTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JediPointsTracker
+//
+//  Created by Amanda Chappell on 4/28/17.
+//  Copyright © 2017 Amanda Chappell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JediPointsTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JediPointsTracker
+//
+//  Created by Amanda Chappell on 4/28/17.
+//  Copyright © 2017 Amanda Chappell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
